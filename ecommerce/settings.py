dccfadb469504c1f2f39c24da5f0a61c022dc528
Hisham1404 <hishammohammed1404@gmail.com>
2023-11-25 22:33:09 +0530

    first commi

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-xav!@g#b!^vbaz0$)0crj@u&02ycg)dm5rfdwtd7gnr6#vgnbi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = ["ecommerce-b9dl.onrender.com"]
 
 
 # Application definition
