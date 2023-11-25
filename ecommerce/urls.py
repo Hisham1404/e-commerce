55d256474d939f71f6838d5c10ad30c0421417ef
Hisham1404 <hishammohammed1404@gmail.com>
2023-11-25 22:29:35 +0530

    ommit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,7 +23,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('',include('paypal.standard.ipn.urls')),
+    
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
